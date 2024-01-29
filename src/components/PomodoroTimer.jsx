d270baafbb645ad927ef00441e74fbf17f0d4d1a
Eder Henrique <ederhscc@gmail.com>
2024-01-29 14:07:29 -0300

    added styles in PomodoroTimer.jsx component with chakra/ui

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+
+import {
+  Box,
+  Button,
+  Text,
+  Input,
+  FormControl,
+  FormLabel,
+  Center,
+  HStack,
+} from "@chakra-ui/react";
 
 const PomodoroTimer = () => {
   return (
-    <div>PomodoroTimer</div>
-  )
-}
+    <Center>
+      <Box width="100%" maxWidth="400px" p="4">
+        <Text fontSize="4xl" mb="4" textAlign="center">
+          10:00
+        </Text>
+        <FormControl mb="4">
+          <FormLabel>Definir tempo(minutos)</FormLabel>
+          <Input type="number" />
+        </FormControl>
+        <HStack>
+          <Button>Iniciar</Button>
+          <Button colorScheme="gray">Reiniciar</Button>
+        </HStack>
+      </Box>
+    </Center>
+  );
+};
 
-export default PomodoroTimer
+export default PomodoroTimer;
